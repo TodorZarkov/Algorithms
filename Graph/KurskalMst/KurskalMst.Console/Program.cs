3274e4d0ce6d77e14bcbf5db80152a8a52a38660
TodorZarkov <t.p.zarkov@gmail.com>
2025-03-23 00:49:04 +0200

    fixed Kurskal with parent array

--- Graph/KurskalMst/KurskalMst.Console/Program.cs
+++ Graph/KurskalMst/KurskalMst.Console/Program.cs
@@ -1,39 +1,58 @@
 ﻿
+
 namespace KurskalMst.Console
 {
     internal class Program
     {
+        private static Dictionary<int, int> parent;
         static void Main(string[] args)
         {
             int numberOfEdges = int.Parse(System.Console.ReadLine());
-            PriorityQueue<(int, int), double> edges = new PriorityQueue<(int, int), double>();
+            List<(int, int, int)> edges = new List<(int, int, int)>();
+            parent = new Dictionary<int, int>();
             for (int i = 0; i < numberOfEdges; i++)
             {
-                int[] line = System.Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int[] line = System.Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
                 int first = line[0];
                 int second = line[1];
-                double weight = line[2];
-                edges.Enqueue((first, second), weight);
+                int weight = line[2];
+                edges.Add((first, second, weight));
+
+                parent[first] = first;
+                parent[second] = second;
             }
 
-            HashSet<int> uniqueNodes = new HashSet<int>();
-            List<(int, int)> forest = new List<(int, int)>();
-            while (edges.Count > 0)
+            List<(int, int, int)> forest = new List<(int, int, int)>();
+            edges = edges.OrderBy(e => e.Item3).ToList();
+            foreach (var edge in edges)
             {
-                var edge = edges.Dequeue();
-                int uniqueCountBefore = uniqueNodes.Count();
-                uniqueNodes.Add(edge.Item1);//it doesn't catch the directed graph case
-                uniqueNodes.Add(edge.Item2);
-                if (uniqueNodes.Count() - uniqueCountBefore > 0)
+
+                int firstNodeRoot = GetRoot(edge.Item1);
+                int secondNodeRoot = GetRoot(edge.Item2);
+                if (firstNodeRoot == secondNodeRoot)
                 {
-                    forest.Add((edge.Item1, edge.Item2));
+                    continue;
                 }
+                parent[firstNodeRoot] = secondNodeRoot;
+                forest.Add(edge);
             }
 
             foreach (var edge in forest)
             {
                 System.Console.WriteLine($"{edge.Item1} - {edge.Item2}");
             }
+        }
+
+        private static int GetRoot(int node)
+        {
+            while (parent[node] != node)
+            {
+                node = parent[node];
+            }
+            return node;
         }
     }
 
